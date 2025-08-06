eeb2ca36eac34e5ae45c6e3c9a221a456ec06b56
EivydasLeo <eivydasleo@gmail.com>
2025-08-06 11:29:00 +0300

    feature: Added fetch logic and updated components with styling.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import styles from "./app.module.scss";
+import { GifCard } from "../GifCard/GifCard";
+
+const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+
+export default function App() {
+    const [gifs, setGifs] = useState([]);
+
+    useEffect(() => {
+        const fetchGifs = async () => {
+            try {
+                const res = await fetch(
+                    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=leisure&limit=12`,
+                );
+                const data = await res.json();
+                setGifs(data.data);
+            } catch (error) {
+                console.error("Klaida parsisiunčiant GIF'us:", error);
+            }
+        };
+
+        fetchGifs();
+    }, []);
+
+    return (
+        <>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>Giphy</h1>
+                <div className={styles.body}>
+                    {gifs.map((gif) => (
+                        <GifCard key={gif.id} gif={gif} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
