135af1cfc3ea082c7447aff0c4c2ffe228a43d24
EivydasLeo <eivydasleo@gmail.com>
2025-08-10 15:20:43 +0300

    refactor: RefreshButton refactor.

--- src/components/RefreshButton/RefreshButton.jsx
+++ src/components/RefreshButton/RefreshButton.jsx
@@ -1,12 +1,11 @@
 import styles from "./refreshbutton.module.scss";
-import refreshIcon from "../../assets/refresh.svg";
 
 export const RefreshButton = ({ onClick, className = "" }) => {
     const text = "Hit here to refresh gifs or press space";
 
     return (
         <button onClick={onClick} className={`${styles.button} ${className}`}>
-            <img src={refreshIcon} alt="refresh" /> {text}
+            <img src="../../../public/svg/refresh.svg" alt="refresh" /> {text}
         </button>
     );
 };
